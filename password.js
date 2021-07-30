49ba87d1fab573fbc72f7d2e78014dfeec5763dd
Blake Nuttall <blakedyblake@gmail.com>
2021-07-30 10:38:38 -0600

    Got rid a do while loop that looped it before I could enter the password. That was weird.

--- password.js
+++ password.js
@@ -1,10 +1,17 @@
 console.log("Welcome to the validator tool")
 let answer = ""
+const readline = require("readline");
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 let success ="Good Job"
 
-do{
-    let password = prompt("Please type in your password")
-    let arr = password.split("");
-    answer = arr.length > 9 ?  success : "Not long enough try again"
-}while(answer !== success)
+    let arr;
+    reader.question("Type your password(10+ characters please)",function(password){
+        arr = password.split("");
+        answer = arr.length > 9 ?  success : "Not long enough try again"
+        console.log(answer)
+    });
+
