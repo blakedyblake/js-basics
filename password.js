ef3de562553be2459b674dfe65a24787b3ea6837
Blake Nuttall <blakedyblake@gmail.com>
2021-07-30 10:18:16 -0600

    I put the .split inthe do while where it belongs not outside where it causes an error

--- password.js
+++ password.js
@@ -1,9 +1,10 @@
 console.log("Welcome to the validator tool")
 let answer = ""
-let arr = password.split("");
+
 let success ="Good Job"
 
 do{
     let password = prompt("Please type in your password")
-    answer = length > 9 ?  success: "Not long enough try again"
+    let arr = password.split("");
+    answer = arr.length > 9 ?  success : "Not long enough try again"
 }while(answer !== success)
